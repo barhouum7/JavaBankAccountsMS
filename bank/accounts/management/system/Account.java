3cfff12912a6cde41e16ad32dff82737ca7bdcb6
barhouum7 <bb.med2000@gmail.com>
2020-12-11 15:38:27 +0100

    Added the Account Class

--- bank/accounts/management/system/Account.java
+++ bank/accounts/management/system/Account.java
@@ -1,7 +1,27 @@
 package bank.accounts.management.system;
 
-public class Account
+public class Account {
 
-{
+    private static int accountId = 0;
+    private double accountBalance;
+    private double maxOverDraft;
+    private boolean isOverDraft = false;
+    private double maxDebit;
+    final Customer accountHolder; // Constant
+
+    /**
+     * 
+     * @param holder
+     * @param balance
+     * @param maxOverDraft
+     * @param maxDebit
+     */
+    public Account(Customer holder, double balance, double maxOverDraft, double maxDebit) {
+        this.accountHolder = holder;
+        this.accountBalance = balance;
+        this.maxOverDraft = maxOverDraft;
+        this.maxDebit = maxDebit;
+        accountId++;
+    }
 
 }
