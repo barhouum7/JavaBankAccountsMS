6fce9bc150ebf7819c2bcbc01639a94a8f45a175
barhouum7 <bb.med2000@gmail.com>
2020-12-12 20:24:26 +0100

    Added some helper methods

--- bank/accounts/management/system/Account.java
+++ bank/accounts/management/system/Account.java
@@ -9,19 +9,30 @@ public class Account {
     private double maxDebit;
     final Customer accountHolder; // Constant
 
-    /**
-     * 
-     * @param holder
-     * @param balance
-     * @param maxOverDraft
-     * @param maxDebit
-     */
     public Account(Customer holder, double balance, double maxOverDraft, double maxDebit) {
         this.accountHolder = holder;
         this.accountBalance = balance;
         this.maxOverDraft = maxOverDraft;
         this.maxDebit = maxDebit;
         accountId++;
+    }
+
+    public Account(Customer holder) {
+        this.accountHolder = holder;
+        this.accountBalance = 0;
+        this.maxOverDraft = -800;
+        this.maxDebit = 1000;
+    }
+
+    public void credit(double balance) {
+        this.accountBalance += balance;
+    }
+
+    public void debit(double balance) {
+        if ((this.accountBalance - balance) < this.maxOverDraft)
+            System.out.println("Unauthorized operation!");
+        else
+            this.accountBalance -= balance;
     }
 
 }
